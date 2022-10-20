1463ee859d8e877cf8d0674609fcac9800ad099a
Arsen <arsen.danylenko@gmail.com>
2022-10-20 17:20:39 +0300

    the main page is done, commit before creating the modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('Hey Arsen!');
+const label = document.querySelector(".form-label");
+const input = document.querySelector("#inputZip");
+
+if (label) {
+   input.addEventListener('click', function (a) {
+      label.classList.add('_pressed');
+   })
+   input.addEventListener('mouseover', function (a) {
+      label.classList.add('_pressed');
+   })
+   input.addEventListener('mouseout', function (a) {
+      label.classList.remove('_pressed');
+   })
+   input.addEventListener('focusout', function (a) {
+      label.classList.remove('_pressed');
+   })
+}
